c2b394a4447120ccdc2cb7efc9f18fcdf97a3170
GBS <GBS@Rands-MacBook-Pro.local>
2017-10-13 14:09:16 +0800

    NPM Setup
    NPM Setup

---
+++ 2/2.js
@@ -0,0 +1,29 @@
+// Calculator
+
+const numbers = [1, 2, 3, 4];
+
+const add = function (x, y){
+  return x+y;
+};
+
+const substract = function (x, y){
+  return x-y;
+};
+
+const multiply = function (x, y){
+  return x*y;
+};
+
+const divide = function (x, y){
+  return x/y;
+};
+
+
+const sum = function (list){
+
+};
+
+const compute = function (a, b, operation){
+  let output = operation (a,b);
+  return output;
+};
---
+++ gulp/gulpfile.js
@@ -0,0 +1,38 @@
+const gulp         = require('gulp');
+const sass         = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps   = require('gulp-sourcemaps');
+const browserSync  = require('browser-sync').create();
+
+
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("src/scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+gulp.task('sass', function () {
+  return gulp.src('src/scss/*.scss')
+  	.pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(browserSync.stream());
+   
+});
+
+gulp.task('default', ['browser-sync']);
+
+
+
+
